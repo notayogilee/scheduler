3395439086dc603e08f087b6bc029c5f39a7c59a
Lee Castelani <notayogilee@gmail.com>
2020-04-16 14:36:24 -0400

    add some transitions for form display or empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,28 @@ import 'components/Appointment/styles.scss';
 import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
+import useVisulaMode from '../../hooks/useVisualMode';
 
-
-
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  // console.log('Appp props', props)
 
-  const display = props.interview
-    ? <Show
-      interviewer={props.interview.interviewer}
-      student={props.interview.student}
-    />
-    : <Empty />
+
+  const { mode, transition, back } = useVisulaMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  function onAdd() {
+    transition(CREATE);
+  }
+
+  function onCancel() {
+    back();
+  }
 
   return (
 
@@ -22,7 +32,19 @@ export default function Appointment(props) {
       <Fragment>
         <Header time={props.time}>
         </Header>
-        {display}
+        {mode === EMPTY && <Empty onAdd={onAdd} />}
+        {mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+          />
+        )}
+        {mode === CREATE &&
+          <Form
+            interviewers={[]}
+            onCancel={onCancel}
+            onSave={props.onSave}
+          />}
       </Fragment>
     </article>
 
